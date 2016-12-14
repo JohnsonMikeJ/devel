fac767bbf9d04421f6642239b2c527044f398b1d
Mike J Johnson <mike@mikejjohnson.com>
2016-12-14 18:09:34 -0500

    Fixed typo

--- vagrant/provisioning-scripts/yum-install-dnsmasq.sh
+++ vagrant/provisioning-scripts/yum-install-dnsmasq.sh
@@ -46,7 +46,7 @@ sudo service network restart
 echo -e "Restarting dnsmasq service ..."
 sudo service dnsmasq restart
 
-echo -e "Test pings (if anything times out you have an issue"
+echo -e "Test pings (if anything times out you have an issue)"
 ping -c 1 localhost
 ping -c 1 test.localhost
 ping -c 1 google.com
